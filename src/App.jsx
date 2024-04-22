0971e750f87d5167714f005859b98e2aee72a370
Malek Kazal <ghazal.malek@gmail.com>
2024-04-22 18:08:12 -0400

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import "./App.css";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import About from "./Pages/About";
+import Skills from "./Pages/Skills";
+import Projects from "./Pages/Projects";
+import Contact from "./Pages/Contact";
+import NotFound from "./Pages/NotFound";
 
 function App() {
-  return (
-    <>
-      <h1>Malek.</h1>
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route index element={<Homepage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/skills' element={<Skills />} />
+        <Route path='/portfolio' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
+      </>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
