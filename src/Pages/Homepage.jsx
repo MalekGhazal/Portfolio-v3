bb090de631ddc6e1bd0e1cafa62c7d09c81aa9a7
Malek Kazal <ghazal.malek@gmail.com>
2024-04-24 18:05:43 -0400

    homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,5 +1,52 @@
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
+import "../Styles/Homepage.css";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const name = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(name.current, {
+      strings: ["Malek", "Kazal."],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <>
+      <div className='bg-malek-yellow h-screen'>
+        <div className='flex flex-col max-w-80 m-auto pt-48'>
+          <h1 className='font-bungee-shade text-malek-green text-9xl ml-1'>
+            <span ref={name} />
+          </h1>
+          <p className='text-5xl font-bungee-normal text-malek-dark mb-10 mt-5 ml-2'>
+            Full Stack <br />
+            <span className='text-4xl'>Developer</span>
+          </p>
+          <div className='flex items-center'>
+            <a href='' target='_blank'>
+              <p className='text-9xl text-malek-white font-bungee-normal'>
+                Blog
+              </p>
+            </a>
+            <div className='typewriter ml-12'>
+              <div className='slide'>
+                <i></i>
+              </div>
+              <div className='paper'></div>
+              <div className='keyboard'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Homepage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "malek-white": "#F5F7F8",
+      "malek-yellow": "#F4CE14",
+      "malek-green": "#495E57",
+      "malek-dark": "#45474B",
+    },
+    fontFamily: {
+      "bungee-shade": ["Bungee Shade", "sans-serif"],
+      "bungee-normal": ["Bungee", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
